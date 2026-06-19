2e85607fadb0e00fb7dc3e92410810b91ffdf1ef
Mohammed Ajmal <ajmalmt67@gmail.com>
2026-06-19 11:31:34 +0530

    in venue service file added getVenueDetail API

--- frontend/src/services/venueService.js
+++ frontend/src/services/venueService.js
@@ -10,3 +10,28 @@ export const getVenues = async (params = {}) => {
 
   return response.data;
 };
+
+
+export const getVenueDetail = async (
+  slug
+) => {
+  const response = await api.get(
+    `/venues/venues/${slug}/`
+  );
+
+  return response.data;
+};
+
+export const getVenueAvailability =
+  async (slug, date) => {
+
+    const response =
+      await api.get(
+        `/venues/${slug}/availability/`,
+        {
+          params: { date },
+        }
+      );
+
+    return response.data;
+};
